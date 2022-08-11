fa89e2012a2fa8fb775c9cb7ee88068071624fc7
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2022-08-11 11:24:38 +0500

    Add files via upload

---
+++ Sports/sportspredict/dashboard/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect, render
+from .forms import DataForm
+from .models import Data
+# Create your views here.
+def index(request):
+    if request.method=='POST':
+        form=DataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-predictions')
+    else:
+        form=DataForm()
+    context={'form':form}
+    return render(request,'dashboard/index.html',context)
+def predictions(request):
+    predicted_sports=Data.objects.all()
+    print( predicted_sports[0].predictions)
+    context={
+        'predicted_sports':predicted_sports,
+    }
+    return render(request,'dashboard/predictions.html',context)
